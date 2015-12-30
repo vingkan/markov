925da638284a982874149089c26099524db3a596
Vinesh Kannan <vingkan@gmail.com>
2015-12-30 14:08:57 -0600

    Added Neil deGrasse Tyson corpus

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,15 @@
 var MarkovChain = require('markovchain-generate');
+var fs = require('fs');
+
+var pokemon = fs.readFileSync(__dirname + '/pokemon.txt', 'utf8');
+//console.log(pokemon);
+
+var neil = fs.readFileSync(__dirname + '/neil-degrasse-tyson.txt', 'utf8');
+//console.log(neil);
 
 var chain = new MarkovChain();
-
-var text = "I wanna be the very best Like no one ever was To catch them is my real test To train them is my cause I will travel across the land Searching far and wide Each pokemon to understand The power that's inside Pokemon! Gotta catch 'em all! It's you and me I know it's my destiny, Pokemon! Oh you're my best friend In a world we must defend Pokemon! A heart so true Our courage will pull us through, You teach me and I'll teach you, Pokemon! Gotta catch'em all Every challenge along the way With courage I will face. I will battle every day To claim my rightful place. Come with me, The time is right, There's no better team. Arm in arm we'll win the fight! It's always been our dream!";
-
-	chain.generateChain(text);
-
+	chain.generateChain(neil);
 var output = chain.generateString();
 	console.log(output);
 
-var fs = require('browserify-fs');
-var path = require('path-browserify');
-
-var file = path.join(__dirname, 'pokemon.txt');
-fs.readFile(file, {encoding: 'utf-8'}, function(error, data){
-	if(error){
-		console.log("Something went horribly wrong: " + error);
-	}
-	console.log(data);
-});
+	//node_modules/.bin/watchify
