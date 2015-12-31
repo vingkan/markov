932119d83fecfd7caa0051fd3d7d82af31e5a2a9
Vinesh Kannan <vingkan@gmail.com>
2015-12-30 23:15:43 -0600

    LinkedIn Law of Incoherency

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,21 @@
 var MarkovChain = require('markovchain-generate');
+var nlp = require('nlp_compromise');
 var fs = require('fs');
 
-var pokemon = fs.readFileSync(__dirname + '/pokemon.txt', 'utf8');
-//console.log(pokemon);
-
+/*var pokemon = fs.readFileSync(__dirname + '/pokemon.txt', 'utf8');
 var neil = fs.readFileSync(__dirname + '/neil-degrasse-tyson.txt', 'utf8');
-//console.log(neil);
+*/
 
-var chain = new MarkovChain();
-	chain.generateChain(neil);
-var output = chain.generateString();
-	console.log(output);
+var kidjkadj = fs.readFileSync(__dirname + '/kidj-kadj.txt', 'utf8');
 
-	document.write(output);
+function markovSays(){
+	var chain = new MarkovChain();
+		chain.generateChain(kidjkadj);
+	var output = chain.generateString();
+		console.log(output);
+		document.write(output);
+	var processed = nlp.pos(output).to_past().text();
+		console.log(processed);
+}
 
-var nlp = require('nlp_compromise');
-
-var processed = nlp.pos(output).to_past().text();
-	console.log(processed);
+window.markovSays = markovSays;
